e9754ebe32c71daa7e30f1af241ea05f7da00030
weber costa <86451517+webersilv4@users.noreply.github.com>
2022-08-02 19:02:13 -0300

    Update Config.php

--- source/Config.php
+++ source/Config.php
@@ -4,7 +4,7 @@
 
 
     // Database
-    define('DSN', 'mysql:host=localhost;dbname=wide-pay;charset=utf8');
-    define('USER', 'root');
-    define('PASSWD', '');
+    define('DSN', 'mysql:host=mysql741.umbler.com:41890;dbname=wide-pay;charset=utf8');
+    define('USER', 'webersilv4');
+    define('PASSWD', '8CxeHBgwoy');
     define('JWT_KEY', '1f3870be274f6c49b3e31a0c6728957f');
