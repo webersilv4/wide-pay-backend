efd0c884c4c356a468b51b329f6cfb3936dcc967
weber costa <86451517+webersilv4@users.noreply.github.com>
2022-08-02 19:12:05 -0300

    Update Config.php

--- source/Config.php
+++ source/Config.php
@@ -1,10 +1,10 @@
 <?php
 
-    define ("URL_BASE", "http://localhost/Projects/project-wide-pay");
+    define ("URL_BASE", "https://wide-pay.herokuapp.com");
 
 
     // Database
-    define('DSN', 'mysql:host=mysql741.umbler.com:41890;dbname=wide-pay;charset=utf8');
+    define('DSN', 'mysql:host=mysql741.umbler.com:41890;dbname=widepay;charset=utf8');
     define('USER', 'webersilv4');
     define('PASSWD', '8CxeHBgwoy');
     define('JWT_KEY', '1f3870be274f6c49b3e31a0c6728957f');
